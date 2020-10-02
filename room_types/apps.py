6cca3b450d43ea4823cef512a82f81041fb9b0dd
Crowiant <nap@liis.su>
2020-10-02 12:43:10 +0300

    clear version

--- room_types/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RoomTypesConfig(AppConfig):
-    name = 'room_types'
--- room_types/urls.py
+++
@@ -1,7 +0,0 @@
-from room_types import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.ListCreateRoomTypesView.as_view()),
-    path('<int:pk>/', views.UpdateDestroyRoomTypesView.as_view())
-]
