3d9d05a0c7471f062c582ff8c63d6966de922a88
Crowiant <nap@liis.su>
2020-10-02 11:06:56 +0300

    new serializers for room type

--- room_types/serializers.py
+++ room_types/serializers.py
@@ -5,8 +5,25 @@ from room_types.models import RoomType
 
 
 class RoomTypeSerializer(serializers.ModelSerializer):
-    office = serializers.PrimaryKeyRelatedField(queryset=Office.objects.all(), required=True)
+    office = serializers.PrimaryKeyRelatedField(queryset=Office.objects.all(), required=False)
+    title = serializers.CharField(max_length=140, required=False)
 
     class Meta:
         model = RoomType
         fields = '__all__'
+
+
+class CreateRoomTypeSerializer(serializers.ModelSerializer):
+    office = serializers.PrimaryKeyRelatedField(queryset=Office.objects.all(), required=True)
+
+    class Meta:
+        model = RoomType
+        exclude = ['pre_defined', ]
+
+
+class ListCreateRoomType(serializers.ModelSerializer):
+    pass
+
+
+class UpdateRoomTypeSerializer(serializers.ModelSerializer):
+    pass
--- room_types/views.py
+++ room_types/views.py
@@ -1,11 +1,13 @@
+from rest_framework import status
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.mixins import UpdateModelMixin, ListModelMixin, CreateModelMixin, DestroyModelMixin
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser, AllowAny
 # Local imports
 from backends.pagination import DefaultPagination
 from backends.mixins import FilterListMixin
-from room_types.serializers import RoomTypeSerializer
+from room_types.serializers import RoomTypeSerializer, CreateRoomTypeSerializer
 from room_types.models import RoomType
 from offices.models import Office
 
@@ -15,8 +17,10 @@ class ListCreateRoomTypesView(GenericAPIView, CreateModelMixin, ListModelMixin):
     serializer_class = RoomTypeSerializer
     queryset = RoomType.objects.all()
     pagination_class = DefaultPagination
+    # permission_classes = (IsAdminUser,)
 
     def post(self, request, *args, **kwargs):
+        self.serializer_class = CreateRoomTypeSerializer
         return self.create(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
@@ -30,9 +34,13 @@ class UpdateDestroyRoomTypesView(GenericAPIView,
     serializer_class = RoomTypeSerializer
     queryset = RoomType.objects.all()
     pagination_class = DefaultPagination
+    # permission_classes = (IsAdminUser,)
 
     def put(self, request, *args, **kwargs):
         return self.update(request, *args, **kwargs)
 
-    def delete(self, request, *args, **kwargs):
+    def delete(self, request, pk=None, *args, **kwargs):
+        self.queryset = RoomType.objects.filter(pre_defined=False)
+        if pk not in [room_type.id for room_type in self.queryset]:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return self.destroy(request, *args, **kwargs)
