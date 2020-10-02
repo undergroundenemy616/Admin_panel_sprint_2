4d301ddb055899079700df16d750970fd53bc27c
Crowiant <nap@liis.su>
2020-10-02 12:44:09 +0300

    clear version

---
+++ room_types/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from room_types.models import RoomType
+# Register your models here.
+
+admin.site.register(RoomType)
---
+++ room_types/models.py
@@ -0,0 +1,16 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from offices.models import Office
+from files.models import File
+
+
+class RoomType(models.Model):
+    title = models.CharField(max_length=140, null=False)
+    office = models.ForeignKey(Office, null=False, on_delete=models.CASCADE)
+    color = models.CharField(max_length=8, default="#0079c1")
+    icon = models.ForeignKey(File, default=None, on_delete=models.SET_NULL, null=True)
+    bookable = models.BooleanField(default=False)
+    work_interval_days = models.IntegerField(validators=[MaxValueValidator(90), MinValueValidator(0)], default=0)
+    work_interval_hours = models.IntegerField(validators=[MaxValueValidator(24), MinValueValidator(0)], default=0)
+    unified = models.BooleanField(default=False)
+    pre_defined = models.BooleanField(default=False)
