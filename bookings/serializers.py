776d2f76d80fcc9bccb942b2833a139aa96a53da
Crowiant <nap@liis.su>
2020-10-08 15:12:28 +0300

    book pre-ready

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -1,7 +1,11 @@
-from datetime import datetime
+import random
+from datetime import datetime, timedelta
+from django.db.models import Q
 from rest_framework import serializers
 # Local imports
 from bookings.models import Booking, Table
+
+MINUTES_TO_ACTIVATE = 15
 
 
 def date_validator(value):
@@ -14,19 +18,43 @@ class BookingSerializer(serializers.ModelSerializer):
     date_from = serializers.DateTimeField(required=True, validators=[date_validator])
     date_to = serializers.DateTimeField(validators=[date_validator])
     table = serializers.PrimaryKeyRelatedField(queryset=Table.objects.all(), required=True)
-    theme = serializers.CharField(max_length=200)
+    theme = serializers.CharField(max_length=200, default="Без темы")
 
     class Meta:
         model = Booking
         fields = ['date_from', 'date_to', 'table', 'theme']
 
-    def create(self, validated_data):
+    def create(self, validated_data, *args, **kwargs):
         model = self.Meta.model
         table = validated_data.pop('table')
+        date_from = validated_data.pop('date_from')
+        date_to = validated_data.pop('date_to')
+        code_gen = random.randint(1000, 9999)
+        if date_to < date_from:
+            raise serializers.ValidationError("Ending time should be larger than the starting one")
+        overflows = Booking.objects.filter(table=table, is_over=False). \
+            filter(Q(date_from__gt=date_to, date_to__lte=date_to) | Q(date_from__lte=date_from, date_to__lt=date_from)
+                   | Q(date_from__gte=date_from, date_to__lte=date_to))
+        if overflows:
+            raise serializers.ValidationError("Table already booked")
+        date_now = datetime.utcnow()
+        if date_now <= date_from:
+            if date_to >= date_now + timedelta(minutes=MINUTES_TO_ACTIVATE):
+                date_activate_until = date_from + timedelta(minutes=MINUTES_TO_ACTIVATE)
+            else:
+                date_activate_until = date_now + timedelta(minutes=MINUTES_TO_ACTIVATE)
+        elif date_to >= date_now + timedelta(minutes=MINUTES_TO_ACTIVATE):
+            date_activate_until = date_now + timedelta(minutes=MINUTES_TO_ACTIVATE)
+        else:
+            date_activate_until = date_to
+        booking = model.objects.create(date_to=date_to, date_from=date_from,
+                                       table=table, date_activate_until=date_activate_until,
+                                       code=code_gen, user=args[0])  # TODO refactor user
 
 
-class BookingForActionSerializer(serializers.ModelSerializer):
+class BookingActionSerializer(serializers.ModelSerializer):
     booking = serializers.PrimaryKeyRelatedField(queryset=Booking.objects.all(), required=True)
+
     # Todo Code
 
     class Meta:
