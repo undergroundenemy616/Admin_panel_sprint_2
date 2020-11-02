d0060aa4c593ed8c54308c4bb91d9d867c3dd631
Crowiant <nap@liis.su>
2020-11-02 15:53:52 +0300

    fix route room_types to type

--- booking_api_django_new/urls.py
+++ booking_api_django_new/urls.py
@@ -64,7 +64,7 @@ urlpatterns = [
     path('rooms', include('rooms.urls_detail')),
     path('floors', include('floors.urls')),
     path('office', include('offices.urls')),
-    path('room_types', include('room_types.urls')),
+    path('type', include('room_types.urls')),
     path('book', include('bookings.urls')),
     path('books', include('bookings.urls_detail')),
     path('book_operator', include('bookings.urls_operator')),
