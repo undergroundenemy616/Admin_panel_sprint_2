2a465dc53adeacdd410287b3e486aedf0865c1b7
Crowiant <nap@liis.su>
2020-11-16 17:28:37 +0300

    table route fixed

--- tables/serializers.py
+++ tables/serializers.py
@@ -68,7 +68,8 @@ class TableSerializer(serializers.ModelSerializer):
 
 class CreateTableSerializer(serializers.ModelSerializer):
     room = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all(), required=True)
-    tags = serializers.ListField(child=serializers.CharField(), validators=[check_table_tags_exists], write_only=True,
+    tags = serializers.ListField(child=serializers.PrimaryKeyRelatedField(queryset=TableTag.objects.all()),
+                                 validators=[check_table_tags_exists], write_only=True,
                                  allow_empty=True, required=False, allow_null=True)
     images = serializers.ListField(child=serializers.PrimaryKeyRelatedField(queryset=File.objects.all()),
                                    write_only=True, allow_empty=True, required=False)
@@ -90,8 +91,12 @@ class CreateTableSerializer(serializers.ModelSerializer):
         model = self.Meta.model
         tags = validated_data.pop('tags', None)
         room = validated_data.pop('room')
+        images = validated_data.pop('images')
         instance = model.objects.create(room=room, **validated_data)
         office_id = instance.room.floor.office.id
+        if images:
+            for image in images:
+                instance.images.add(image)
         if tags:
             tags_queryset = TableTag.objects.filter(title__in=tags, office_id=office_id)
             instance.tags.set(tags_queryset)
@@ -102,9 +107,11 @@ class UpdateTableSerializer(CreateTableSerializer):
     room = serializers.PrimaryKeyRelatedField(queryset=Room.objects.all(), required=False)
     description = serializers.CharField(required=False)
     title = serializers.CharField(required=False)
-    tags = serializers.ListField(child=serializers.CharField(), validators=[check_table_tags_exists], write_only=True,
+    tags = serializers.ListField(child=serializers.PrimaryKeyRelatedField(queryset=TableTag.objects.all()),
+                                 validators=[check_table_tags_exists], write_only=True,
                                  allow_empty=True, allow_null=True, required=False)
-    images = serializers.ListField(child=serializers.CharField(), write_only=True, allow_empty=True, required=False)
+    images = serializers.ListField(child=serializers.PrimaryKeyRelatedField(queryset=File.objects.all()),
+                                   write_only=True, allow_empty=True, required=False)
 
     class Meta:
         model = Table
