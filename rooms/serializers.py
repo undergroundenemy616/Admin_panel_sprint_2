88b125dab6c877cd9c712ad22784105d6f0a0fd5
Crowiant <nap@liis.su>
2020-11-17 16:52:14 +0300

    fix room serializer

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -34,7 +34,7 @@ class RoomSerializer(serializers.ModelSerializer):
         response['type'] = room_type['title']
         response['room_type_color'] = room_type['color']
         response['room_type_unified'] = room_type['unified']
-        response['room_type_icon'] = [FileSerializer(instance=image).data for image in room_type['icon'].all()]
+        response['room_type_icon'] = [FileSerializer(instance=room_type['icon']).data]
         response['tables'] = [TableSerializer(instance=table).data for table in instance.tables.all()]
         response['capacity'] = instance.tables.count()
         response['marker'] = instance.room_marker if hasattr(instance, 'room_marker') else None
