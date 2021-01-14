d5c98ed300a0d07829ca9488aad151112bf355af
Crowiant <nap@liis.su>
2021-01-14 18:18:05 +0300

    restore

--- rooms/views.py
+++ rooms/views.py
@@ -1,17 +1,22 @@
 from typing import Dict, Optional
 
 from django.db.models import Q
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import GenericAPIView, get_object_or_404
 from rest_framework.mixins import UpdateModelMixin, RetrieveModelMixin, CreateModelMixin, \
     DestroyModelMixin, Response, status
 from rest_framework.request import Request
+from rest_framework import filters
+from datetime import datetime
 
 from core.pagination import DefaultPagination
 from core.permissions import IsAdmin
 from core.mixins import FilterListMixin
 from rooms.models import Room, RoomMarker
 from rooms.serializers import RoomSerializer, FilterRoomSerializer, CreateRoomSerializer, UpdateRoomSerializer, \
-    RoomMarkerSerializer
+    RoomMarkerSerializer, SwaggerSer
+from tables.models import Table
+from bookings.models import Booking
 
 
 class RoomsView(FilterListMixin,
@@ -20,6 +25,8 @@ class RoomsView(FilterListMixin,
     serializer_class = RoomSerializer
     queryset = Room.objects.all()
     pagination_class = DefaultPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'type__title', 'description']
 
     permission_classes = (IsAdmin, )
 
@@ -43,12 +50,87 @@ class RoomsView(FilterListMixin,
             del mapped[item]
         return mapped
 
+    @swagger_auto_schema(query_serializer=SwaggerSer)
     def get(self, request, *args, **kwargs):
-        if request.query_params.get('search'):
-            self.queryset = Room.objects.filter(Q(title__icontains=request.query_params.get('search'))
-                                                | Q(type__title__icontains=request.query_params.get('search'))
-                                                | Q(description__icontains=request.query_params.get('search')))
-        return self.list(request, *args, **kwargs)
+        response = []
+        rooms = Room.objects.all()
+
+        if request.query_params.get('office'):
+            rooms = rooms.filter(floor__office_id=request.query_params.get('office'))
+        if request.query_params.get('floor'):
+            rooms = rooms.filter(floor_id=request.query_params.get('floor'))
+        if request.query_params.get('zone'):
+            rooms = rooms.filter(zone_id=request.query_params.get('zone'))
+
+        for room in rooms:
+            response.append(RoomSerializer(instance=room).data)
+
+        if request.query_params.get('date_to') and request.query_params.get('date_from'):
+            date_from = datetime.strptime(request.query_params.get('date_from'), '%Y-%m-%dT%H:%M:%S.%f')
+            date_to = datetime.strptime(request.query_params.get('date_to'), '%Y-%m-%dT%H:%M:%S.%f')
+            if date_from > date_to:
+                return Response({"detail": "Not valid data"}, status=status.HTTP_400_BAD_REQUEST)
+            for room in response:
+                bookings = Booking.objects.filter(
+                    (
+                            (Q(date_from__gte=date_from) & Q(date_from__lt=date_to))
+                            |
+                            (Q(date_from__lte=date_from) & Q(date_to__gte=date_to))
+                            |
+                            (Q(date_to__gt=date_from) & Q(date_to__lte=date_to))
+                    )
+                )
+                for booking in bookings:
+                    room_tables = room['tables'][:]
+                    for table in room['tables']:
+                        if table.get('id') == str(booking.table_id):
+                            room_tables.remove(table)
+                    room['tables'] = room_tables
+
+        if request.query_params.get('range_to') and request.query_params.get('range_from'):
+            not_in_range = []
+            range_to = int(request.query_params.get('range_to'))
+            range_from = int(request.query_params.get('range_from'))
+            if range_to is not None and range_from > range_to:
+                return Response({"detail": "Not valid data"}, status=status.HTTP_400_BAD_REQUEST)
+            for room in response:
+                if (range_to is not None and len(room.get('tables')) not in range(range_from, range_to + 1)) or \
+                        (range_to is None and len(room.get('tables')) < range_from):
+                    not_in_range.append(room)
+            for room in not_in_range:
+                response.remove(room)
+
+        if request.query_params.get('marker') and int(request.query_params.get('marker')) == 1:
+            with_marker = []
+            for room in response:
+                if room['marker']:
+                    with_marker.append(room)
+            response = with_marker
+
+        if request.query_params.get('image') is not None:
+            if int(request.query_params.get('image')) == 1:
+                with_image = []
+                for room in response:
+                    if room['images']:
+                        with_image.append(room)
+                response = with_image
+            elif int(request.query_params.get('image')) == 0:
+                without_image = []
+                for room in response:
+                    if not room['images']:
+                        without_image.append(room)
+                response = without_image
+
+        suitable_tables = 0
+
+        for room in response:
+            suitable_tables += len(room.get('tables'))
+
+        response_dict = {
+            'results': response,
+            'suitable_tables': suitable_tables
+        }
+        return Response(response_dict, status=200)
 
     def post(self, request, *args, **kwargs):
         self.permission_classes = (IsAdmin, )
