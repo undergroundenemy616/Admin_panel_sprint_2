2852f7e0878842c36599ad333410d52fafb1031a
Pavel Chuykin <chpi@liis.su>
2021-02-09 17:48:07 +0300

    https://liis.atlassian.net/browse/AA2-1302 https://liis.atlassian.net/browse/AA2-1303 https://liis.atlassian.net/browse/AA2-1304

--- booking_api_django_new/settings.py
+++ booking_api_django_new/settings.py
@@ -14,7 +14,7 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path='booking_api_django_new/environments/' + os.environ.get('BRANCH', default='plchkn') + '.env')
+load_dotenv(dotenv_path='booking_api_django_new/environments/' + os.environ.get('BRANCH', default='master') + '.env')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
