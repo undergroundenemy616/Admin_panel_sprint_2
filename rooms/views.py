0409760d4f5a66971784edf3fde1125dac1bb804
Pavel Chuykin <chpi@liis.su>
2021-03-25 09:29:51 +0300

    fix for /room, remove table without table marker, remove 'booked table'

--- rooms/views.py
+++ rooms/views.py
@@ -151,13 +151,14 @@ class RoomsView(ListModelMixin,
                 ).filter(table__room__pk=uuid.UUID(room['id'])).select_related('table').values_list('table__id', flat=True)
                 room_tables = room['tables'][:]
                 for table in room['tables']:
-                    if (not table.get('marker') and not room['room_type_unified']) or uuid.UUID(table.get('id')) in bookings:
+                    if not table.get('marker') and not room['room_type_unified']:
+                        room_tables.remove(table)
+                        continue
+                    if uuid.UUID(table.get('id')) in bookings:
                         table['is_avialable'] = False
                         room['suitable_tables'] -= 1
                     else:
                         table['is_avialable'] = True
-                    if room['room_type_unified'] and uuid.UUID(table.get('id')) in bookings:
-                        room['booked_table'] = table
                 room['tables'] = room_tables
                 # for booking in bookings:
                 #     room_tables = room['tables'][:]
