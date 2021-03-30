a7bbf7f0dfaf0a68a7e7c202f698e45f3e429026
Crowiant <nap@liis.su>
2021-03-30 18:35:12 +0300

    view_statistics devided into two views

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from bookings import views
 from bookings import views_actions
 from bookings import views_statistics
+from bookings import views_statistics_FD
 
 urlpatterns = [
     path('', views.BookingsView.as_view()),
@@ -13,6 +14,6 @@ urlpatterns = [
     path('/fast', views.CreateFastBookingsView.as_view()),
     path('/room_type_stats', views_statistics.BookingStatisticsRoomTypes.as_view()),
     path('/employee_stats', views_statistics.BookingEmployeeStatistics.as_view()),
-    path('/future', views_statistics.BookingFuture.as_view()),
-    path('/dashboard', views_statistics.BookingStatisticsDashboard.as_view())
+    path('/future', views_statistics_FD.BookingFuture.as_view()),
+    path('/dashboard', views_statistics_FD.BookingStatisticsDashboard.as_view())
 ]
