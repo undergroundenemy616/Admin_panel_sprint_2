ec2691f4e6ee3edc628c9a45fc8ad323c6d5b6ad
Pavel Chuykin <chpi@liis.su>
2021-07-23 09:35:33 +0300

    remove self revoke for make_booking_over in bookings

--- bookings/tasks.py
+++ bookings/tasks.py
@@ -275,7 +275,7 @@ def transfer_task_to_redis():
 
             func_name = '_'.join(job.job_id.split('_')[:-1])
             task_id = str(job.parameters['uuid'])
-            globals()[func_name].apply_async(args=[i for i in job.parameters.values()], eta=time,
+            globals()[func_name].apply_async(args=[i for i in job.parameters.values()], eta=job.time_execute,
                                              task_id=func_name+'_'+task_id)
             logger.info(msg=f'Add task: {func_name}_{task_id}')
     except Exception as e:
