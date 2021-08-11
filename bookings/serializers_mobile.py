3a8fbd03bc40dfd96d1a72e9462aa5afbb02f4d2
Александр Демидов <dag@liis.su>
2021-08-11 15:09:24 +0300

    Added same function for mobile part of application

--- bookings/serializers_mobile.py
+++ bookings/serializers_mobile.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime, timedelta, timezone
 
 import pytz
@@ -5,6 +6,8 @@ from django.core.exceptions import ValidationError as ValErr
 from django.core.validators import validate_email
 from django.db.transaction import atomic
 from django.utils.timezone import now
+from exchangelib import Credentials, Configuration, DELEGATE, CalendarItem, Account as Ac
+from exchangelib.items import SEND_TO_ALL_AND_SAVE_COPY
 from rest_framework import serializers, status
 from rest_framework.exceptions import ValidationError
 
@@ -319,6 +322,40 @@ class MobileMeetingGroupBookingSerializer(serializers.ModelSerializer):
                 my_booking_id = str(b.id)
         response = MobileGroupBookingSerializer(instance=group_booking).data
         response['id'] = my_booking_id
+        if self.validated_data['room'].exchange_email:
+            required_attendees = []
+            for user in self.validated_data['users']:
+                required_attendees.append(user.email if user.email else user.user.email)
+            credentials = Credentials(os.environ['EXCHANGE_ADMIN_LOGIN'], os.environ['EXCHANGE_ADMIN_PASS'])
+            config = Configuration(server=os.environ['EXCHANGE_SERVER'], credentials=credentials)
+            account_exchange = Ac(primary_smtp_address=os.environ['EXCHANGE_ADMIN_LOGIN'], config=config,
+                                  autodiscover=False, access_type=DELEGATE)
+            date_to = self.validated_data['date_to']
+            date_from = self.validated_data['date_from']
+            office = Office.objects.get(id=self.validated_data['room'].floor.office_id)
+            time_zone = pytz.timezone(office.timezone).utcoffset(datetime.now())
+            local_date_from = date_from + time_zone
+            local_date_to = date_to + time_zone
+            if self.context['request'].headers.get('Language', None) == 'ru':
+                message_title = "Приглашение на встречу"
+                message_body = f"Вы были приглашены на встречу, которая пройдёт в {self.validated_data['room'].title}."
+            else:
+                message_title = "Meeting invitation"
+                message_body = f"You have been invited to a meeting in {self.validated_data['room'].title}."
+            exchange_event = CalendarItem(
+                account=account_exchange,
+                folder=account_exchange.calendar,
+                start=datetime(local_date_from.year, local_date_from.month, local_date_from.day,
+                               local_date_from.hour, local_date_from.minute,
+                               tzinfo=account_exchange.default_timezone),
+                end=datetime(local_date_to.year, local_date_to.month, local_date_to.day,
+                             local_date_to.hour, local_date_to.minute, tzinfo=account_exchange.default_timezone),
+                subject=message_title,
+                body=message_body,
+                required_attendees=required_attendees,
+                location=self.validated_data['room'].exchange_email
+            )
+            exchange_event.save(send_meeting_invitations=SEND_TO_ALL_AND_SAVE_COPY)
         return response
 
 
