06235e699fbd979edd20940d16628d26b739cba6
Антон Ниточкин <antonnitockin@192-168-1-149.local>
2021-08-16 09:36:03 +0300

    fixes for group_booking

--- bookings/models.py
+++ bookings/models.py
@@ -175,24 +175,40 @@ class Booking(models.Model):
         try:
             if self.table.room.type.unified and self.table.room.office_panels.exists():
                 if GLOBAL_DATE_FROM_WS.get(f'{self.table.id}') == self.date_from.date():
-                    result_for_date = self.create_response_for_date_websocket()
-                    try:
-                        asyncio.run(self.websocket_notification_by_date(result_for_date))
-                        print('Sending create timeline block')
-                    except Exception as e:
-                        print('-----ERROR--CREATE--BY--DATE---', e)
+                    if self.group_booking is None:
+                        result_for_date = self.create_response_for_date_websocket()
+                        try:
+                            asyncio.run(self.websocket_notification_by_date(result_for_date))
+                            print('Sending create timeline block')
+                        except Exception as e:
+                            print('-----ERROR--CREATE--BY--DATE-SIMPLY-----BOOKING--', e)
+                    elif self.group_booking.author == self.user.id:
+                        result_for_date = self.create_response_for_date_websocket()
+                        try:
+                            asyncio.run(self.websocket_notification_by_date(result_for_date))
+                            print('Sending create timeline block')
+                        except Exception as e:
+                            print('-----ERROR--CREATE--BY--DATE---GROUP---BOOKING---', e)
                 else:
                     pass
                 if ((GLOBAL_DATETIME_FROM_WS[f'{self.table.id}'] < self.date_to <= GLOBAL_DATETIME_TO_WS[f'{self.table.id}'])
                         or (GLOBAL_DATETIME_FROM_WS[f'{self.table.id}'] <= self.date_from < GLOBAL_DATETIME_TO_WS[f'{self.table.id}'])
                         or (GLOBAL_DATETIME_FROM_WS[f'{self.table.id}'] >= self.date_from >= GLOBAL_DATETIME_TO_WS[f'{self.table.id}'])
                         and (GLOBAL_DATETIME_FROM_WS[f'{self.table.id}'] < self.date_to)):
-                    result_for_datetime = self.create_response_for_datetime_websocket()
-                    try:
-                        asyncio.run(self.websocket_notification_by_datetime(result_for_datetime))
-                        print('Sending create meeting block')
-                    except Exception as e:
-                        print('-----ERROR--CREATE--BY--DATETIME---', e)
+                    if self.group_booking is None:
+                        result_for_datetime = self.create_response_for_datetime_websocket()
+                        try:
+                            asyncio.run(self.websocket_notification_by_datetime(result_for_datetime))
+                            print('Sending create meeting block')
+                        except Exception as e:
+                            print('-----ERROR--CREATE--BY--DATETIME---', e)
+                    elif self.group_booking.author == self.user.id:
+                        result_for_datetime = self.create_response_for_datetime_websocket()
+                        try:
+                            asyncio.run(self.websocket_notification_by_datetime(result_for_datetime))
+                            print('Sending create meeting block')
+                        except Exception as e:
+                            print('-----ERROR--CREATE--BY--DATETIME---', e)
         except Exception as e:
             print('------------ERROR--CREATE---WITH-----WS----LOGIC-------', e)
         super(self.__class__, self).save(*args, **kwargs)
