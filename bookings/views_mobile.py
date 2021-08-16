cc4189fa135bc5fd7ac82b7403b84a113542f578
Антон Ниточкин <antonnitockin@192-168-1-149.local>
2021-08-16 14:55:27 +0300

    fixes for group meeting booking now author last finish booking

--- bookings/views_mobile.py
+++ bookings/views_mobile.py
@@ -159,10 +159,20 @@ class MobileGroupMeetingBookingViewSet(viewsets.ModelViewSet):
         if account == instance.author:
             if instance.bookings.filter(user=instance.author)[0].status != 'waiting':
                 for booking in instance.bookings.all():
-                    booking.make_booking_over()
+                    if booking.user.id == account.id:
+                        pass
+                    else:
+                        booking.make_booking_over()
+                last_author_booking = instance.bookings.filter(user=account.id)
+                last_author_booking.make_booking_over()
                 return Response(status=status.HTTP_200_OK)
             for booking in instance.bookings.all():
-                booking.make_booking_over()
+                if booking.user.id == account.id:
+                    pass
+                else:
+                    booking.make_booking_over()
+            last_author_booking = instance.bookings.filter(user=account.id)
+            last_author_booking.make_booking_over()
             self.perform_destroy(instance)
             return HttpResponse(status=204)
         else:
